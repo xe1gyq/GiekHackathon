e152bc63b89f810847a11f263ef70d71f908d0dc
Abraham Arce <xe1gyq@gmail.com>
2016-06-20 23:10:29 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ main.py
@@ -0,0 +1,23 @@
+import time
+
+import pyupm_biss0001 as upmMotion
+import pyupm_grove as grove
+import pyupm_i2clcd as lcd
+
+button = grove.GroveButton(8)
+light = grove.GroveLight(0)
+motion = upmMotion.BISS0001(3)
+lcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
+
+while True:
+    print 'Button value: ', button.value()
+    print 'Light value: ', light.raw_value()
+    print 'Motion value: ', motion.value()
+
+    lcd.setCursor(0,0)
+    lcd.setColor(255, 0, 0)
+    lcd.write('Hello World')
+
+    time.sleep(1)
+
+del button
--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@ echo "Welcome to Grove Indoor Environment Kit Hackathon"
 git clone https://github.com/xe1gyq/GiekHackathon.git
 cd GiekHackathon/
 pip install pip --upgrade
-sh requirements.opkg
-pip install -r requirements.pip
 
 echo
 echo "Now go to GiekHackathon directory to get started!"

f5dc8d4657cb4bb881412312cb58a073d33c4340
Abraham Arce <xe1gyq@gmail.com>
2016-06-20 22:43:49 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,12 @@
+echo "Welcome to Grove Indoor Environment Kit Hackathon Edition"
+
+git clone https://github.com/xe1gyq/GiekHackathon.git
+cd GiekHackathon/
+pip install pip --requests
+sh requirements.opkg
+pip install -r requirements.pip
+
+echo
+echo "Now go to GiekHackathon directory to get started!"
+echo
+echo "Happy GiekHackathon'ing"

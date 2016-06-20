746788976d7de1c801c358fcaaf8ac9edaee952e
Abraham Arce <xe1gyq@gmail.com>
2016-06-20 23:12:51 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import pyupm_i2clcd as lcd
 button = grove.GroveButton(8)
 light = grove.GroveLight(0)
 motion = upmMotion.BISS0001(3)
+relay = grove.GroveRelay(2)
 lcd = lcd.Jhd1313m1(0, 0x3E, 0x62)
 
 while True:
@@ -14,10 +15,14 @@ while True:
     print 'Light value: ', light.raw_value()
     print 'Motion value: ', motion.value()
 
+    relay.on()
+
     lcd.setCursor(0,0)
     lcd.setColor(255, 0, 0)
     lcd.write('Hello World')
 
     time.sleep(1)
+
+    relay.off()
 
 del button

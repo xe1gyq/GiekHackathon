ccb8fa065d9e07459879116aa58c77bef5dda65e
Abraham Arce <xe1gyq@gmail.com>
2016-06-21 15:30:52 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 echo "Welcome to Grove Indoor Environment Kit Hackathon"
 
+cat << EOM > /etc/opkg/base-feeds.conf
+src/gz all http://repo.opkg.net/edison/repo/all
+src/gz edison http://repo.opkg.net/edison/repo/edison
+src/gz core2-32 http://repo.opkg.net/edison/repo/core2-32
+EOM
+
+opkg update
+
 git clone https://github.com/xe1gyq/GiekHackathon.git
 cd GiekHackathon/
 pip install pip --upgrade
